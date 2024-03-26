fedfbc1b4369b1e23d230280b124e126a078ae84
bigbootybandit <35828699+q35u437@users.noreply.github.com>
2024-03-26 15:27:24 +0530

    Update using_2_stacks.cpp

--- trees/iterative_postorder/using_2_stacks.cpp
+++ trees/iterative_postorder/using_2_stacks.cpp
@@ -3,9 +3,6 @@
 //now swap left and right calls then it will be root, right, left
 //now just reverse it then: left, right, root
 //hoooray!!! we got postorder traversal with the help of two stacks
-//why this post order iterative approach is a but tougher because if we try to trace the output then, we can clearly see that after we are done with left and right
-//child we can't tell who is the parent node.
-
 vector<int> postorderTraversal(TreeNode* root) {
     //using two stacks
     if(!root){

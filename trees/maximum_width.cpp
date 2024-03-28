d05e63765d16276d88899f9303fa5a976e41cdd0
bigbootybandit <35828699+q35u437@users.noreply.github.com>
2024-03-28 16:01:01 +0530

    Update maximum_width.cpp

--- trees/maximum_width.cpp
+++ trees/maximum_width.cpp
@@ -1,19 +1,19 @@
 int widthOfBinaryTree(TreeNode* root) {
-    int width = INT_MIN;
-    queue<pair<TreeNode*, long long>> q;
+    unsigned long long width = 0;
+    queue<pair<TreeNode*, unsigned long long>> q;
     q.push({root, 0});
     while(!q.empty()){
         int length = q.size();
-        int first,last;
+        unsigned long long first,last;
         for(int i=0;i<length;i++){
             if(i==0) first = q.front().second;
             if(i==length-1) last = q.front().second;
             auto it = q.front();
             q.pop();
             if(it.first->left)
-                q.push({it.first->left, 2*(it.second-first)+1});
+                q.push({it.first->left, 2*(it.second-1)+1});
             if(it.first->right)
-                q.push({it.first->right, 2*(it.second-first)+2});
+                q.push({it.first->right, 2*(it.second-1)+2});
         }
         width = max(width,last-first+1);
     }

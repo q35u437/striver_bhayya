872b99674fd6041c3d00c5a13736eb9613c58cb3
bigbootybandit <35828699+q35u437@users.noreply.github.com>
2024-08-04 11:00:41 +0530

    Update adj_matrix.cpp

--- graphs/adj_matrix.cpp
+++ graphs/adj_matrix.cpp
@@ -5,13 +5,20 @@ int main() {
     int nodes, edges;
     cin >> nodes >> edges;
 
-    vector<vector<int>> graph(nodes, vector<int>(nodes, 0));
+    int graph[nodes][nodes] = {};
 
     for(int edge=0;edge < edges; edge++) {
         int u, v;
         cin >> u >> v;
         graph[u-1][v-1] = 1;
         graph[v-1][u-1] = 1;
+    }
+
+    for(int i = 0; i<nodes; i++) {
+        for(int j=0; j < nodes; j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout<<endl;
     }
     return 0;
 }

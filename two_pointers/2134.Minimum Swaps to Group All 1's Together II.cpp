bc4626a17b1ca604f1076e66987ffd250c6bace0
bigbootybandit <35828699+q35u437@users.noreply.github.com>
2024-08-05 12:41:25 +0530

    Create 2134.Minimum Swaps to Group All 1's Together II.cpp

---
+++ two_pointers/2134.Minimum Swaps to Group All 1's Together II.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int occurences = count(nums.begin(), nums.end(), 1);
+        if(not occurences) return 0;
+        int left = 0, right = occurences-1;
+        int length = nums.size();
+        for(int index = 0; index < length; index++){
+            nums.push_back(nums[index]);
+        }
+        int maybe = 0;
+        for(int index = left; index <= right;index++){
+            maybe += (nums[index]==0);
+        }
+
+        int answer = INT_MAX;
+        while(left < length){
+            while(right - left + 1 < occurences){
+                right = right + 1;
+                maybe += (nums[right]==0);
+            }
+            answer = min(answer, maybe);
+            maybe -= (nums[left]==0);
+            left++;
+        }
+        return answer;
+    }
+};
